d0177054de79faf9918db50b951a4bc2c6209025
mynmss03@gmail.com <mynmss03@gmail.com>
2022-04-12 16:40:17 +0530

    j

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from telegram.ext.updater import Updater
-
-from telegram.update import Update
-
-from telegram.ext.callbackcontext import CallbackContext
-
-from telegram.ext.commandhandler import CommandHandler
-
-from telegram.ext.messagehandler import MessageHandler
-
-from telegram.ext.filters import Filters
-
-updater =("5177532718:AAGNPPGUfspoeBGZeNzPQXusu-14qMB-mlU",use_context=True);
-
-def start(update: Update, context: CallbackContext):update.message.reply_text("Enter the text you want to show to the user whenever they start the b bot");
-
+from telegram import *from telegram.ext import * from requests import * updater = Updater(token="2002660437:AAHmXhLwNJYfc0FmADZl4IImjcHVtvnhYww") dispatcher = updater.dispatcher randomPeopleText = "Random Person"randomImageText = "Random Image" randomPeopleUrl = "https://thispersondoesnotexist.com/image"randomPImageUrl = "https://picsum.photos/1200" likes = 0dislikes = 0 allowedUsernames = [] def startCommand(update: Update, context: CallbackContext): buttons = [[KeyboardButton(randomImageText)], [KeyboardButton(randomPeopleText)]] context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons)) def messageHandler(update: Update, context: CallbackContext): if update.effective_chat.username not in allowedUsernames: context.bot.send_message(chat_id=update.effective_chat.id, text="You are not allowed to use this bot") return if randomPeopleText in update.message.text: image = get(randomPeopleUrl).content if randomImageText in update.message.text: image = get(randomPImageUrl).content if image: context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(image, caption="")]) buttons = [[InlineKeyboardButton("👍", callback_data="like")], [InlineKeyboardButton("👎", callback_data="dislike")]] context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text="Did you like the image?") def queryHandler(update: Update, context: CallbackContext): query = update.callback_query.data update.callback_query.answer() global likes, dislikes if "like" in query: likes +=1 if "dislike" in query: dislikes +=1 print(f"likes => {likes} and dislikes => {dislikes}") dispatcher.add_handler(CommandHandler("start", startCommand))dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))dispatcher.add_handler(CallbackQueryHandler(queryHandler)) updater.start_polling()

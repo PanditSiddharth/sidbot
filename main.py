ef4cd97fe5c7c22bd847bee123d328e5f5148e20
mynmss03@gmail.com <mynmss03@gmail.com>
2022-04-12 16:20:30 +0530

    hi

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ from telegram.ext.filters import Filters
 
 updater = Updater("5177532718:AAH_Rri2dqFT1w9pyAgzwZPjTSy75t8_JxM",use_context=True)
 
-def start(update: Update, context: CallbackContext):update.message.reply_text("Enter the text you want to show to the user whenever they start the bot")
+def start(update: Update, context: CallbackContext):update.message.reply_text("Enter the text you want to show to the user whenever they start the b bot")
 

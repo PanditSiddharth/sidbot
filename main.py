fa07f292c8828532ad293ae6d3b7ea3769fa2ce2
mynmss03@gmail.com <mynmss03@gmail.com>
2022-04-12 18:47:46 +0530

    sidbot

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from telegram import *from telegram.ext import * from requests import * updater = Updater(token="5177532718:AAGNPPGUfspoeBGZeNzPQXusu-14qMB-mlU") dispatcher = updater.dispatcher randomPeopleText = "Random Person"randomImageText = "Random Image" randomPeopleUrl = "https://thispersondoesnotexist.com/image"randomPImageUrl = "https://picsum.photos/1200" likes = 0dislikes = 0 allowedUsernames = [] def startCommand(update: Update, context: CallbackContext): buttons = [[KeyboardButton(randomImageText)], [KeyboardButton(randomPeopleText)]] context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons)) def messageHandler(update: Update, context: CallbackContext): if update.effective_chat.username not in allowedUsernames: context.bot.send_message(chat_id=update.effective_chat.id, text="You are not allowed to use this bot") return if randomPeopleText in update.message.text: image = get(randomPeopleUrl).content if randomImageText in update.message.text: image = get(randomPImageUrl).content if image: context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(image, caption="")]) buttons = [[InlineKeyboardButton("👍", callback_data="like")], [InlineKeyboardButton("👎", callback_data="dislike")]] context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text="Did you like the image?") def queryHandler(update: Update, context: CallbackContext): query = update.callback_query.data update.callback_query.answer() global likes, dislikes if "like" in query: likes +=1 if "dislike" in query: dislikes +=1 print(f"likes => {likes} and dislikes => {dislikes}") dispatcher.add_handler(CommandHandler("start", startCommand))dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))dispatcher.add_handler(CallbackQueryHandler(queryHandler)) updater.start_polling()
+from telegram import *
+from telegram.ext import *
+
+bot = Bot("5177532718:AAGNPPGUfspoeBGZeNzPQXusu-14qMB-mlU")
+
+updater = Updater("5177532718:AAGNPPGUfspoeBGZeNzPQXusu-14qMB-mlU",use_context=True)
+
+dispatcher = updater.dispatcher
+def function(update:Update,context:CallbackContext):
+    bot.send_message(
+    chat_id = update.effective_chat.id,
+    text = "hi hello"
+);
+start_value = CommandHandler('start',  function)
+dispatcher.add_handler(start_value)
+updater.start_polling()
